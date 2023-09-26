b0aa15b89d2f597662c6d998801d9201206af5f5
Sergey Trusilo <svt@krononsoft.com>
2023-09-26 09:55:11 +0300

    Init

---
+++ Assets/Driver.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+
+    void Start()
+    {
+      transform.Rotate(0,0, 45);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
